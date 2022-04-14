3f14e1f2767e24206123ea096a0ab2e08c1d1de7
Damilare Darmie Akinlaja <darmie@akinlaja.com>
2022-04-14 11:04:39 +0100

    Initial commit

---
+++ linc/linc_empty.h
@@ -0,0 +1,19 @@
+#pragma once
+
+//hxcpp include should always be first    
+#ifndef HXCPP_H
+#include <hxcpp.h>
+#endif
+
+//include other library includes as needed
+// #include "../lib/____"
+
+namespace linc {
+
+    namespace empty {
+
+        extern int example();
+
+    } //empty namespace
+
+} //linc
